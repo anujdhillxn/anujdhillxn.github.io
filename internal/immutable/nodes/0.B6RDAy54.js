3ab52275e39f21254e9612358c22656562a8e449
Anuj Dhillon <anuj.dhillon.14@gmail.com>
2025-10-06 01:02:32 +0530

    Updates

---
+++ internal/immutable/nodes/0.B6RDAy54.js
@@ -0,0 +1,1 @@
+import"../chunks/Bzak7iHL.js";import{v as n,w as a,x as p}from"../chunks/CL7feWYS.js";import{s}from"../chunks/D1YzBjOQ.js";const i=!0,u=Object.freeze(Object.defineProperty({__proto__:null,prerender:i},Symbol.toStringTag,{value:"Module"}));function d(o,r){var e=n(),t=a(e);s(t,()=>r.children),p(o,e)}export{d as component,u as universal};
